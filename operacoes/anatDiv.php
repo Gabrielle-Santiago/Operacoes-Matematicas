d11008ec73e1b34368ea63c1baf028881c7788e8
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2023-11-01 11:33:51 -0300

    Commit do HTML e organização das pastas

---
+++ operacoes/anatDiv.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Divisão</title>
+</head>
+<body>
+    <h1>Anatomia de Divisão</h1>
+
+    <label for="anatDiv">
+        <input type="number" name="anatDiv1" id="num1">
+    </label>
+    <label for="anatDiv">
+        <input type="number" name="anatDiv2" id="num2">
+    </label>
+
+    <div>
+        <h1>Resultado</h1>
+    </div>
+</body>
+</html>
