20159c149f27d50b2b9a416f049a8903b7ddfe8b
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2023-11-04 21:41:58 -0300

    Criou a parte do PHP

--- operacoes/anatDiv.php
+++ operacoes/anatDiv.php
@@ -8,15 +8,31 @@
 <body>
     <h1>Anatomia de Divisão</h1>
 
-    <label for="anatDiv">
-        <input type="number" name="anatDiv1" id="num1">
-    </label>
-    <label for="anatDiv">
-        <input type="number" name="anatDiv2" id="num2">
-    </label>
+    <form action="http://localhost/Projetos/Operacoes-Matematicas/Operacoes-Matematicas/operacoes/anatDiv.php" method="get">
+
+        <label for="num1">
+            <input type="number" name="anatDiv1" id="num1">
+        </label>
+        <label for="num2">
+            <input type="number" name="anatDiv2" id="num2">
+        </label>
+
+        <input type="submit" value="Enviar">
+    </form>
 
     <div>
         <h1>Resultado</h1>
     </div>
+
+    <?php 
+        if (isset($_GET['anatDiv1']) && isset($_GET['anatDiv2'])) {
+            $numUM = $_GET['anatDiv1'];
+            $numDOIS = $_GET['anatDiv2'];
+        } 
+        if ($numUM != 0 && $numDOIS) {
+            $resul = $numUM / $numDOIS;
+            echo "O resultado é: $resul";
+        }
+    ?>
 </body>
 </html>
