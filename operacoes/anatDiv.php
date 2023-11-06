138f2b35645d51d7740c42f880474e161051d632
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2023-11-06 20:42:07 -0300

    Consetou a sintexe da operação

--- operacoes/anatDiv.php
+++ operacoes/anatDiv.php
@@ -10,11 +10,11 @@
 
     <form action="http://localhost/Projetos/Operacoes-Matematicas/Operacoes-Matematicas/operacoes/anatDiv.php" method="get">
 
-        <label for="num1">
-            <input type="number" name="anatDiv1" id="num1" min="1" required>
+        <label for="anatDiv1">
+            <input type="number" name="anatDiv1" id="anatDiv1" min="1" required>
         </label>
-        <label for="num2">
-            <input type="number" name="anatDiv2" id="num2" min="1" required>
+        <label for="anatDiv2">
+            <input type="number" name="anatDiv2" id="anatDiv2" min="1" required>
         </label>
 
         <input type="submit" value="Enviar">
@@ -31,9 +31,13 @@
          $resul = ($numUM / $numDOIS);
          $restDiv = ($numUM % $numDOIS);
 
-         //Resultado da divisão
-        
-         echo "$numUM" . " $numDOIS" . " $restDiv" . " $resul";
+         //Quantidade de casas decimais
+         if ($numUM != 0 && $numDOIS != 0) {
+            $resul = number_format($resul, 2);
+
+         echo "$numUM $numDOIS $restDiv $resul";
+         }
+
     ?>
 </body>
 </html>
